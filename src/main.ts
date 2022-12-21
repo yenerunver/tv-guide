a90a05b3963058c8e08566921b241130b5dfb14d
Yener Unver <yener.unver@takeaway.com>
2022-12-21 22:02:11 +0300

    Implement Vuex store

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { createApp } from "vue";
+import { createStore } from 'vuex'
 import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+
+const store = createStore({
+    state () {
+        return {
+            country: {
+                name: "United States",
+                code: "US",
+                timezone: "America/New_York"
+            }
+        }
+    }
+})
+
 
 const app = createApp(App);
 
 app.use(router);
 
 app.mount("#app");
+
+app.use(store)
