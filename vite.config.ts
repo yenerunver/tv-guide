1cceed6e888ff288ffb321c3a0f1da1d385f5afc
Yener Unver <yener.unver@takeaway.com>
2022-12-30 22:18:16 +0100

    add githubio deploy key

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,9 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  base: process.env.BASE_URL,
+  build: {
+    sourcemap: true,
+    manifest: true,
+  },
 });
